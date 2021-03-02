61f51bdaf535b8690d202cc6c48190e43196afb0
Ashish Patel <ashishsushilpatel@gmail.com>
2021-03-02 14:20:39 +0530

    feat: new website design

--- build_database.py
+++ build_database.py
@@ -1,7 +1,13 @@
 from datetime import timezone
+import httpx
 import git
+import os
 import pathlib
+from urllib.parse import urlencode
 import sqlite_utils
+from pydriller import RepositoryMining
+from sqlite_utils.db import NotFoundError
+import time
 
 
 root = pathlib.Path(__file__).parent.resolve()
@@ -9,12 +15,10 @@ root = pathlib.Path(__file__).parent.resolve()
 
 def created_changed_times(repo_path, ref="master"):
     created_changed_times = {}
-    repo = git.Repo(repo_path, odbt=git.GitDB)
-    commits = reversed(list(repo.iter_commits(ref)))
-    for commit in commits:
-        dt = commit.committed_datetime
-        affected_files = list(commit.stats.files.keys())
-        for filepath in affected_files:
+    for commit in RepositoryMining("https://github.com/ashishdotme/til.ashish.me/").traverse_commits():
+        dt = commit.committer_date
+        for modified_file in commit.modifications:
+            filepath = modified_file.new_path
             if filepath not in created_changed_times:
                 created_changed_times[filepath] = {
                     "created": dt.isoformat(),
@@ -37,17 +41,58 @@ def build_database(repo_path):
         fp = filepath.open()
         title = fp.readline().lstrip("#").strip()
         body = fp.read().strip()
+        slug = filepath.stem
         path = str(filepath.relative_to(root))
-        url = "https://github.com/ashishdotme/til/blob/master/{}".format(path)
+        path_slug = path.replace("/", "_")
+        url = "https://github.com/ashishdotme/til.ashish.me/blob/master/{}".format(path)
+        try:
+            row = table.get(path_slug)
+            previous_body = row["body"]
+            previous_html = row["html"]
+        except (NotFoundError, KeyError):
+            previous_body = None
+            previous_html = None
         record = {
-            "path": path.replace("/", "_"),
+            "path": path_slug,
             "topic": path.split("/")[0],
             "title": title,
             "url": url,
+            "slug": slug,
             "body": body,
         }
+        if (body != previous_body) or not previous_html:
+            retries = 0
+            response = None
+            while retries < 3:
+                headers = {}
+                if os.environ.get("GITHUB_TOKEN"):
+                    headers = {
+                        "authorization": "Bearer {}".format(os.environ["GITHUB_TOKEN"])
+                    }
+                response = httpx.post(
+                    "https://api.github.com/markdown",
+                    json={
+                        # mode=gfm would expand #13 issue links and suchlike
+                        "mode": "markdown",
+                        "text": body,
+                    },
+                    headers=headers,
+                )
+                if response.status_code == 200:
+                    record["html"] = response.text
+                    print("Rendered HTML for {}".format(path))
+                    break
+                else:
+                    print("  sleeping 60s")
+                    time.sleep(60)
+                    retries += 1
+            else:
+                assert False, "Could not render {} - last response was {}".format(
+                    path, response.headers
+                )
         record.update(all_times[path])
-        table.insert(record)
+        with db.conn:
+            table.upsert(record, alter=True)        
     if "til_fts" not in db.table_names():
         table.enable_fts(["title", "body"])
 
